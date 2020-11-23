404b8f9d6f800eb9e7cbec703e0e332e2fd0ad53
jefby <jef199006@gmail.com>
2020-11-23 09:38:27 +0000

    [feat]: update install.sh with local vimrc

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -SL# git.io/tvimrc -o ~/.vimrc
+#curl -SL# git.io/tvimrc -o ~/.vimrc
+
+cp vimrc ~/.vimrc
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
 curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 ~/.vimpkg/bin/apt-vim install -y https://github.com/scrooloose/nerdtree.git
